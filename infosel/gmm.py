f9b9e2d84964278a08ce6a20b70332ca6e79f462
Felipe Maia Polo <felipemaiapolo@gmail.com>
2020-10-30 21:56:13 -0300

    Update gmm.py

--- infosel/gmm.py
+++ infosel/gmm.py
@@ -12,7 +12,7 @@ def check_array(X, name="X", dim=2):
     if not (type(X)==np.ndarray) or len(X.shape)!=dim:
             raise ValueError(name+" should be a {:}-dimensional Numpy array.".format(dim))
             
-def gmm_scores(X_train, X_val, k, covariance_type='full', random_state=42):
+def gmm_scores(X_train, X_val, k, covariance_type='full', reg_covar=1e-06, random_state=42):
     
     '''
     This function trains a GMM and evaluate it in a holdout set using the mean log_likelihood of samples
@@ -26,11 +26,11 @@ def gmm_scores(X_train, X_val, k, covariance_type='full', random_state=42):
     Output: - log_likelihood in the holdout set
     '''
     
-    clf = mixture.GaussianMixture(n_components=k, covariance_type=covariance_type, random_state=random_state)
+    clf = mixture.GaussianMixture(n_components=k, covariance_type=covariance_type, reg_covar=reg_covar, random_state=random_state)
     clf.fit(X_train)
     return clf.score(X_val)
 
-def get_gmm(X, y, y_cat=False, covariance_type='full', max_comp=20, val_size=0.33, random_state=42):
+def get_gmm(X, y, y_cat=False, max_comp=20, val_size=0.33, covariance_type='full', reg_covar=1e-06, random_state=42):
     
     '''
     This function trains a GMM and evaluate it in a holdout set using the mean log_likelihood of samples
@@ -58,11 +58,11 @@ def get_gmm(X, y, y_cat=False, covariance_type='full', max_comp=20, val_size=0.3
         for c in classes:
             #Selecting number of components
             X_gmm_train, X_gmm_val, _, _=train_test_split(X[y==c], X[y==c], test_size=val_size, random_state=random_state)
-            scores=np.array([gmm_scores(X_gmm_train, X_gmm_val, k, covariance_type=covariance_type, random_state=random_state) for k in list(range(1, max_comp, 1))])
+            scores=np.array([gmm_scores(X_gmm_train, X_gmm_val, k, covariance_type=covariance_type, reg_covar=reg_covar,  random_state=random_state) for k in list(range(1, max_comp, 1))])
             k_star=np.argmax(scores)+1
 
             #Training GMMs
-            gmm[c] = mixture.GaussianMixture(n_components=k_star, covariance_type=covariance_type, random_state=random_state)
+            gmm[c] = mixture.GaussianMixture(n_components=k_star, covariance_type=covariance_type, reg_covar=reg_covar, random_state=random_state)
             gmm[c].fit(X[y==c])
         
         return gmm #it is a dictionary of GMMs
@@ -73,12 +73,12 @@ def get_gmm(X, y, y_cat=False, covariance_type='full', max_comp=20, val_size=0.3
         X_gmm_train, X_gmm_val, y_gmm_train, y_gmm_val = train_test_split(X, y, test_size=val_size, random_state=random_state)
         Z_gmm_train=np.hstack((y_gmm_train.reshape((-1,1)), X_gmm_train))
         Z_gmm_val=np.hstack((y_gmm_val.reshape((-1,1)), X_gmm_val))
-        scores=np.array([gmm_scores(Z_gmm_train, Z_gmm_val, k, covariance_type=covariance_type, random_state=random_state) for k in list(range(1, max_comp, 1))])
+        scores=np.array([gmm_scores(Z_gmm_train, Z_gmm_val, k, covariance_type=covariance_type, reg_covar=reg_covar, random_state=random_state) for k in list(range(1, max_comp, 1))])
         k_star=np.argmax(scores)+1
         
         #Training GMM
         Z = np.hstack((y.reshape((-1,1)),X))
-        gmm = mixture.GaussianMixture(n_components=k_star, covariance_type=covariance_type, random_state=random_state)
+        gmm = mixture.GaussianMixture(n_components=k_star, covariance_type=covariance_type, reg_covar=reg_covar, random_state=random_state)
         gmm.fit(Z)
         
         return gmm #it is a GMM
